60930cc236f0d2a63c4c85562eebe821f2bd3a51
Madalin_467 <alexandrumadalin.chitu@ulbsibiu.ro>
2025-04-08 21:47:06 +0300

    Problemele 1 si 2 laborator 6 lucrate aproape integral la ora de laborator

---
+++ src/Laborator6Paradigme/Problema1/main.java
@@ -0,0 +1,11 @@
+import Laborator6Paradigme.Problema1.IntCalculator;
+
+public static void main(String[] args) {
+    IntCalculator calculator = new IntCalculator(10);
+    int result = calculator.add(5).subtract(3).multiply(2).result();
+    System.out.println("a) " + result);
+}
+
+
+
+
